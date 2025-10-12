ccfcf2d9ba654b6be2695efaf9937d5846249fa8
Noxiiie <noxtraditor@gmail.com>
2025-10-12 13:34:11 +0100

    Upload getDbcLevel function

---
+++ ZS Scripting Library/Functions/getDbcLevel.js
@@ -0,0 +1,11 @@
+/** Returns player's dbc level
+ * @param {IPlayer} player 
+ * @returns {Int} - player's dbc level
+ */
+function getDbcLevel(player)
+{
+    var stats = player.getDBCPlayer().getAllAttributes();
+    var totalStats = -55;
+    for (var i = 0; i < stats.length; i++) totalStats += stats[i];
+    return Math.floor(totalStats / 5);
+}
