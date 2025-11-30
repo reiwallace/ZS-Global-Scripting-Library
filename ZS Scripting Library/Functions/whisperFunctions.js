329fed62da4e529e37a978f6f87fe220ef7afee2
reiwallace <reiwallace@yahoo.com>
2025-11-30 14:24:05 +0000

    Add whisper and whisperAll to library

---
+++ ZS Scripting Library/Functions/whisperFunctions.js
@@ -0,0 +1,22 @@
+/** Mimics a whisper message
+ * @param {IPlayer} player 
+ * @param {String} from - Sending name
+ * @param {String} message 
+ */
+function whisper(player, from, message) {
+    if(typeof player == "string") player = API.getPlayer("player")
+    if(!lib.isPlayer(player)) return;
+    player.sendMessage("\u00A76[" + from + "§r\u00A76 -> \u00A7cme\u00A76]§r " + message);
+}
+
+/** Mimics a whisper message and sends to every player currently online
+ * @param {String} from - Sending name
+ * @param {String} message 
+ */
+function whisperAll(from, message) {
+    var players = world.getAllServerPlayers();
+    for (var i = 0; i < players.length; i++) {
+        if(!players[i]) continue;
+        lib.whisper(players[i], from, message);
+    }
+}
