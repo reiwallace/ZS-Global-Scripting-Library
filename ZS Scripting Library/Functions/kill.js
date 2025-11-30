05f32d350a9ab9e67ac706fcc0def7fb052fd699
reiwallace <reiwallace@yahoo.com>
2025-11-30 14:57:09 +0000

    Add string input support for kill function

--- ZS Scripting Library/Functions/kill.js
+++ ZS Scripting Library/Functions/kill.js
@@ -3,6 +3,7 @@
  * @returns Boolean - If the player was killed
  */
 function kill(player){
+    if(typeof player == "string") player = API.getPlayer(player);
     if(!isPlayer(player)) return false;
     API.executeCommand(player.world, "kill " + player.getName());
     return true;
--- ZS Scripting Library/ZS Scripting Library.js
+++ ZS Scripting Library/ZS Scripting Library.js
@@ -348,6 +348,7 @@ function isInRange(anchorPos, targetPos, range) {
  * @returns Boolean - If the player was killed
  */
 function kill(player){
+    if(typeof player == "string") player = API.getPlayer(player);
     if(!isPlayer(player)) return false;
     API.executeCommand(player.world, "kill " + player.getName());
     return true;
